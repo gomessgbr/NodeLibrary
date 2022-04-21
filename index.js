798343c5c8a6f3b24985ac007e7e98867216fbc4
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-04-21 14:17:36 -0300

    add delete rote and todo

--- index.js
+++ index.js
@@ -14,5 +14,17 @@ app.post("/obras", (req, res) => {
   return res.json(books);
 });
 
+app.delete("/obras/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  //   books.splice(id,1)
+  console.log(books);
+  return res.send();
+});
+
+// Todo
+/* - Terminar a rota delete
+   - E fazer a rota de put
+*/
 
 app.listen(3000);
