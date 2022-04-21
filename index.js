f90aaa74a0c21b3f86c3edc7a14dc51af77ae456
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-04-21 13:52:08 -0300

    add obras endpoint for create a new obra

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
-
 const app = express();
 app.use(express.json());
+
 const books = [];
 
 app.get("/obras", (req, res) => {
   return res.json(books);
 });
+
+app.post("/obras", (req, res) => {
+  const { id, titulo, editora, foto } = req.body;
+  books.push(id, titulo, editora, foto);
+  return res.json(books);
+});
+
 
 app.listen(3000);
