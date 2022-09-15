0741feda1765cfa244ff96d3cba63ceb9bb5d089
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-09-15 19:14:29 -0300

    feat: using json

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 app.use(express.json());
-const books = require("./books.json");
-// let books = {};
+// const books = require("./books.json");
+const books1 = [];
 
-app.get("/obras", (_, res) => {
-  return res.json(books);
-});
+// app.get("/obras", (_, res) => {
+//   return res.json(books);
+// });
 
 app.post("/obras", (req, res) => {
   const { id, titulo, editora, foto } = req.body;
-  books[id] = { id, titulo, editora, foto };
+  books1.push({ id: id, titulo: titulo, editora: editora, foto: foto });
+  fs.writeFileSync("./books.json", JSON.stringify(books1), {
+    encoding: "utf8",
+  });
+  // books[id] = { id, titulo, editora, foto };
+  console.log(books1);
   return res.status(200).json("Criado com sucesso");
 });
 
-app.delete("/obras/:id", (req, res) => {
-  const id = req.params.id;
-  let newBooks = books.filter((item) => {
-    if (!item[id]) {
-      return item;
-    }
-  });
+// app.delete("/obras/:id", (req, res) => {
+//   const id = req.params.id;
+//   let newBooks = books.filter((item) => {
+//     if (!item[id]) {
+//       return item;
+//     }
+//   });
 
-  books = newBooks;
-  return res.send(newBooks);
-});
+//   books = newBooks;
+//   return res.send(newBooks);
+// });
 
-app.put("/obras/:id", (req, res) => {
-  const id = req.params.id;
-  if (id) {
-  }
-});
-
-// Todo
-/* - Terminar a rota delete
-   - E fazer a rota de put
-*/
+// app.put("/obras/:id", (req, res) => {
+//   const id = req.params.id;
+//   if (id) {
+//   }
+// });
 
 app.listen(3000);
